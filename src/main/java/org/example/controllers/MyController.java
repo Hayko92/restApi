f5f281d0b9beabb8a6247e8c6814321711f66cd8
Hayk <hajko92@mail.ru>
2021-05-04 10:47:56 +0400

    added exception handling

--- src/main/java/org/example/controllers/MyController.java
+++ src/main/java/org/example/controllers/MyController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.example.services.EmployeeService;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -21,14 +22,14 @@ public class MyController {
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("employees")
+    @GetMapping("/employees")
     public List<Employee> showAllEmployees() throws IOException {
         List<Employee> allEmployees = employeeService.getAllEmployees();
 
         return allEmployees;
     }
 
-    @GetMapping("employees/{id}")
+    @GetMapping("/employees/{id}")
     public Employee getEmployeeById(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
         if (employee == null) {
@@ -37,7 +38,22 @@ public class MyController {
         return employee;
     }
 
+@PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+}
+    @PutMapping("/employees")
+    public Employee updatewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        employeeService.deleteEmployee(id);
 
+        return "Employee with id " + id + " have been deleted";
+    }
 
 }
 
